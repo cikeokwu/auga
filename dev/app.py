dd09d89e1cd53d1d1f27dbe0d3ecaeb3e9d7b48e
ChristianIkeokwu <christianthrone@gmail.com>
2019-02-02 20:22:18 -0500

    added app config

--- dev/app.py
+++ dev/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask import Blueprint,url_for,render_template
 from src.metrics_api import getVolatility
+from config import Config
 
 
 app = Flask(__name__)
-
+app.config.from_object(Config)
 
 @app.route('/')
 @app.route("/index")
