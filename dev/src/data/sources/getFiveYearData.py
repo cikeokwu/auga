dd5efd70a9fd3a501f5da629b7cb6fdf2dbaada3
Robot-Tumas <--replace-all>
2019-02-16 11:14:45 -0500

    remade get5yhistorical for GPU support-- works great~

--- dev/src/data/sources/getFiveYearData.py
+++ dev/src/data/sources/getFiveYearData.py
@@ -2,7 +2,6 @@ import pandas as pd
 import pandas_datareader.data as web
 import pandas_datareader
 import datetime
-import numpy as np
 from iexfinance import get_available_symbols, Stock
 import iexfinance
 
@@ -14,23 +13,20 @@ totalTickers = len(stockSymbols)
 
 
 def get5yHistorical(stockSymbols, totalTickers):
-    bigDF = Stock(stockSymbols[0],output_format="pandas").get_chart(range='5y').transpose()
+   	bigDF = Stock(stockSymbols[0],output_format="pandas").get_chart(range='5y')
     bigDF['ticker']=stockSymbols[0]
-    bigDF.index= [bigDF['ticker'],bigDF.index]
     for i in range(1,totalTickers):
         try:
             print(" iteration:",i," ","ticker: ",stockSymbols[i], 'percentage:',i/totalTickers*100,end='\r')
-            trial=pd.DataFrame(Stock(stockSymbols[i],output_format="pandas").get_chart(range='5y').transpose())
+            trial=pd.DataFrame(Stock(stockSymbols[i],output_format="pandas").get_chart(range='5y'))
             trial['ticker']=stockSymbols[i]
-            trial.index= [trial['ticker'],trial.index]
             bigDF = bigDF.append(trial)
         except KeyError:
-            print('this key had an error:', stockSymbols[i], 'at position:',i)
+            print('\n this key had an error:', stockSymbols[i], 'at position:',i)
             pass
         
     return bigDF
+result=get5yHistorical(stockSymbols,totalTickers)
 
-result=get5yHistorical(stockSymbols,totalTickers)	
-
-result.to_csv('resulting5YearData.csv',index=[result.index])
+result.to_csv('iexRaw.csv')
 
