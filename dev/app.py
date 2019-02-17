c3a30aa42c03a38298596ba97c4c0a8b3b4c35bd
Robot-Tumas <--replace-all>
2019-02-17 07:26:51 -0500

    wooohooo

--- dev/app.py
+++ dev/app.py
@@ -30,7 +30,9 @@ def index():
 def about():
 	return render_template('about.html')
 
-
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
 
 @app.route('/plot/',methods=['POST','GET'])
 def plot():
