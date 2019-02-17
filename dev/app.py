da1746b644fd03a9c4be8b76f485e182e90fbec2
Robot-Tumas <--replace-all>
2019-02-17 03:45:12 -0500

    flask plot functionality integrated

--- dev/app.py
+++ dev/app.py
@@ -1,34 +1,51 @@
-from flask import Flask, render_template, redirect, flash
-from flask import Blueprint,url_for,render_template
-from src.metrics_api import getVolatility
-from config import Config
-from flask_bootstrap import Bootstrap
-# Things needed to be imported for forms:
-from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, SubmitField
-from wtforms.validators import DataRequired
+from flask import Flask, render_template, request
+import pandas as pd
+from bokeh.resources import INLINE
+from bokeh.embed import components
+from bokeh.util.string import encode_utf8
+from src.data.sources.generateDynamicCandleStick import generateCandleStick
 
 
 app = Flask(__name__)
-Bootstrap(app) # allow bootstrap extension
-app.config.from_object(Config)
 
-class MyForm(FlaskForm):
-    ticker = StringField('ticker', validators=[DataRequired()])
-    date = DateField('date', format='%Y-%m-%d', validators=[DataRequired()])
-    submit = SubmitField('Go')
+#Im making this an absolute path, replace with dynamic path, its a hackathon
+datapath = "~/Desktop/projects/python/auga/dev/src/data/sources/"
+# Create the main plot
 
+
+# Index page
 @app.route('/')
-@app.route("/index", methods=['GET', 'POST'])
-def submit():
-    form = MyForm()
-    if form.validate_on_submit():
-        return redirect('/result')
-    return render_template('index.html', form=form)
+def index():
+	# Determine the selected feature
+	
+	# Create the plot
+	
+	return render_template("index.html")
 
-@app.route("/result")
-def result():
-    return render_template('result.html')
 
+@app.route('/plot')
+def plot():
+	currentTickerName = request.args.get("feature_name")
+	if currentTickerName == None:
+		currentTickerName = "SPY"
+
+	plot = generateCandleStick(currentTickerName,datapath)
+	js_resources = INLINE.render_js()
+	css_resources = INLINE.render_css()	
+	# Embed plot into HTML via Flask Render
+	script, div = components(plot)
+
+	html = render_template(
+	'plot.html',
+	plot_script=script,
+	plot_div=div,
+	js_resources=js_resources,
+	css_resources=css_resources,
+	)
+
+	return encode_utf8(html)
+# With debug=True, Flask server will auto-reload 
+# when there are code changes
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(port=5000, debug=True)
+
